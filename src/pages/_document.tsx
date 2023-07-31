cd0b0ac6b17318a421544c6801050febee8f0ab4
Raffay Sajjad <raffaysajjad.apple@gmail.com>
2023-07-31 11:18:18 +0500

    Google site verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="ri3wjCZH-kpm-qABHxMs3Z0HGLWnLzHhylh6WbqR3qw"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
